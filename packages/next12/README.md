7d6ddbaaeb25f58c0dea441cf7ccd9459a4c9665
moonheekim0118 <hellomooneekim@gmail.com>
2024-09-21 18:01:29 +0900

    chore: 패키지 생성

---
+++ packages/next12/README.md
@@ -0,0 +1,11 @@
+# next12
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build next12` to build the library.
+
+## Running unit tests
+
+Run `nx test next12` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/next12/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'next12',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/next12',
+};
